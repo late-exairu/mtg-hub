e3ac303be78e95a44ce2e8336c78e67edcc0b306
Oleg Shelest <lateexairu@gmail.com>
2024-03-28 11:08:47 +0200

    Add banned cards section to App component

--- src/components/BannedCards.tsx
+++ src/components/BannedCards.tsx
@@ -3,12 +3,6 @@ import data from "@/data/data.json";
 export default function BannedCards() {
   return (
     <>
-      <h3 className="text-2xl font-bold">Banned cards</h3>
-      <p className="mb-4">
-        Despite being part of legal sets, the following cards are explicitly not
-        allowed in decks for this format.
-      </p>
-
       <ul className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {data.bannedCards.map((card) => (
           <li key={card.name}>
