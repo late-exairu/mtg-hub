d9f3f8b6f6c3222adae993d93afac8ec9ad698b2
Oleg Shelest <lateexairu@gmail.com>
2024-04-08 20:38:52 +0300

    Add next set and next rotation

--- src/components/StandardSets.tsx
+++ src/components/StandardSets.tsx
@@ -24,6 +24,11 @@ export default function StandardSets(props: StandardSets) {
     return unreleasedSets[0];
   };
 
+  const getNextRotation = () => {
+    const lastBlock = data.standard[data.standard.length - 1];
+    return lastBlock.sets[0];
+  };
+
   const getTimeToRelease = (date: string) => {
     const currentDate = new Date();
     const releaseDate = new Date(date);
@@ -36,6 +41,7 @@ export default function StandardSets(props: StandardSets) {
     return `${months > 0 ? `${months} months` : ""} ${days} days`;
   };
 
+  const setWithRotation = getNextRotation();
   const nextSet = getNextSetToRelease();
 
   return (
@@ -84,27 +90,7 @@ export default function StandardSets(props: StandardSets) {
         ))}
       </div>
 
-      <div className="text-sm mt-4 flex gap-5">
-        <div className="flex relative items-center pl-[50px]">
-          <i
-            className={`ss ss-${nextSet.code} flex text-4xl size-[42px] items-center justify-center absolute left-0 -translate-y-1/2 top-1/2`}
-          ></i>
-          <p className="flex flex-col">
-            {nextSet.name} [{nextSet.code.toUpperCase()}]
-            <span className="italic block">
-              release in {getTimeToRelease(nextSet.releaseDate)}
-              {/* {getTimeToRelease("2024-08-02")} */}
-            </span>
-          </p>
-          {/* <p className="flex items-center">
-            Next rotation(
-            <i
-              className={`${`ss ss-one relative text-2xl lg:text-3xl xl:text-4xl -lg:top-1 mb-4 lg:mb-1 xl:mb-0 lg:size-[51px] xl:size-[63px] size-[37px] lg:-top-2 inline-flex items-center justify-center`}`}
-            ></i>
-            ) release in{" "}
-            <span className="bg-uncommon mr-1 inline-block rounded-[2px]"></span>
-          </p> */}
-        </div>
+      <div className="text-sm mt-4 grid grid-cols-2 md:flex gap-5">
         <div className="">
           <p className="flex items-center">
             <span className="size-3 bg-uncommon mr-1 inline-block rounded-[2px]"></span>{" "}
@@ -124,6 +110,38 @@ export default function StandardSets(props: StandardSets) {
             <span className="size-3 mr-1 inline-block rounded-[2px] bg-card shadow-sm shadow-ring hover:bg-secondary opacity-40"></span>{" "}
             - set is not released
           </p>
+        </div>
+      </div>
+
+      <div className="text-sm mt-4 flex gap-5">
+        <div className="flex flex-col">
+          <h3 className="text-base font-bold">Next set</h3>
+          <div className="flex relative items-center pl-[50px]">
+            <i
+              className={`ss ss-${nextSet.code} flex text-4xl size-[42px] items-center justify-center absolute left-0 -translate-y-1/2 top-1/2`}
+            ></i>
+            <p className="flex flex-col">
+              {nextSet.name} [{nextSet.code.toUpperCase()}]
+              <span className="italic block">
+                release in {getTimeToRelease(nextSet.releaseDate)}
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col">
+          <h3 className="text-base font-bold">Next rotation</h3>
+          <div className="flex relative items-center pl-[50px]">
+            <i
+              className={`ss ss-${setWithRotation.code} flex text-4xl size-[42px] items-center justify-center absolute left-0 -translate-y-1/2 top-1/2`}
+            ></i>
+            <p className="flex flex-col">
+              {setWithRotation.name} [{setWithRotation.code.toUpperCase()}]
+              <span className="italic block">
+                next rotation in {getTimeToRelease(setWithRotation.releaseDate)}
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
