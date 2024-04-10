6ef906e0493020dd74571844a69580d0414ddfc0
Oleg Shelest <lateexairu@gmail.com>
2024-04-10 21:47:09 +0300

    Update BannedCards component styling and layout

--- src/components/BannedCards.tsx
+++ src/components/BannedCards.tsx
@@ -2,17 +2,17 @@ import data from "@/data/data.json";
 
 export default function BannedCards() {
   return (
-    <ul className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-5">
+    <ul className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {data.bannedCards.map((card) => (
-        <li key={card.name} className="flex flex-col">
+        <li key={card.name} className="flex mx-auto sm:mx-0 max-w-72 flex-col">
           <a href={card.scryfallUrl} target="_blank" className="mx-auto">
             <img
               src={card.scryfallImage}
               alt={card.name}
-              className="aspect-[0.714] w-full max-w-64 inline-block mb-2"
+              className="aspect-[0.714] w-full inline-block"
             />
           </a>
-          <div>
+          <div className="-mt-2 pt-4 border border-t-0 rounded-b-[5px] border-slate-300 p-3">
             <h4 className="text-lg font-medium">{card.name}</h4>
             <p className="text-sm">{card.reason}</p>
           </div>
