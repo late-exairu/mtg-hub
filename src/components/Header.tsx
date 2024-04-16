aa8fd9082cbcef778c07a35f4077544ba3c6d3b1
Oleg Shelest <lateexairu@gmail.com>
2024-04-16 16:46:01 +0300

    Update Header component styling and layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <div className="container relative z-10">
-      <h1 className="text-3xl font-black border-b">MTGHub</h1>
+      <h1 className="text-3xl font-black text-center border-b border-slate-300 mx-auto">
+        MTGHub
+      </h1>
     </div>
   );
 }
